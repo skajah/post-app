716151aa4fa342c76f9160bf253f94361afd3c32
Samuel Kajah <kajah.samuel@gmail.com>
2021-03-19 12:02:31 -0500

    Separation of concerns; logging

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,13 @@
-const config = require('config');
-const mongoose = require('mongoose');
+const winston = require('winston');
 const express = require('express');
-const bodyParser = require('body-parser');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-const comments = require('./routes/comments');
-const posts = require('./routes/posts');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const cors = require('cors');
-
-if (!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined');
-  process.exit(1);
-}
-
-mongoose
-  .connect('mongodb://localhost/post-app')
-  .then(() => console.log('Connected to "post-app" db...'))
-  .catch((err) => console.log('Error in connecting: ', err.message));
-
 const app = express();
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(cors());
-
-// routes
-app.use('/api/comments', comments);
-app.use('/api/posts', posts);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
+require('./startup/logging')();
+require('./startup/config')();
+require('./startup/validation')();
+require('./startup/db')();
+require('./startup/routes')(app);
 
 app.listen(4000, () => {
-  console.log('Listening on port 4000...');
+  winston.info('Listening on port 4000...');
 });
